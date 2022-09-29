cf52eb08dff2b5e354de551e232405be9a230d1a
Juan <57921215+jumorap@users.noreply.github.com>
2022-09-29 09:44:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import { graphqlHTTP } from "express-graphql";
 import express from "express";
 
 import { INFO_PERSONAL } from "./sia_infoPersonal/index.js";
-import { INFO_ACADEMICA } from "./sia_infoAcademica/index.js";
 import { SESSION } from "./sia_session/index.js";
 
 
@@ -11,7 +10,6 @@ const app = express()
 // Generate the GraphQL endpoint at /info_personal using the schema and the resolver
 app.use('/info_personal', graphqlHTTP(INFO_PERSONAL));
 app.use('/session', graphqlHTTP(SESSION));
-app.use('/info_academica', graphqlHTTP(INFO_ACADEMICA));
 
 /*
 TODO: Write here your GraphQL endpoint as follows:
